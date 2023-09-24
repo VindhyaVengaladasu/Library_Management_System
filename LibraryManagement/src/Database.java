d672893d924d3326aecf90e20f6a8aa68a4557c2
vindh <vindh@Divya>
2023-09-24 19:03:19 +0530

    firstcommit

---
+++ LibraryManagement/src/Database.java
@@ -0,0 +1,29 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+private static Connection conn = null;
+    
+    public static Connection getConnection(){
+    
+        if(conn == null){
+        
+            try {
+                
+            	Class.forName("com.mysql.cj.jdbc.Driver");  
+                conn=DriverManager.getConnection(  
+                		"jdbc:mysql://localhost:3306/librarymsdb", "root", "Vinnu@2020");  
+                
+            } catch (Exception e) {
+                System.out.println("ERROR: "+e.getMessage());
+                e.printStackTrace();
+            }
+            
+        }
+        return conn;
+    }
+
+}
+
+
